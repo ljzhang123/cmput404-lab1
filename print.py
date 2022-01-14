88567eb814928ad6d9bb72ebafc05cd54256ed48
Lan Zhang <ljzhang@cs.ualberta.ca>
2022-01-14 15:36:02 -0700

    downloads itself and prints itself

--- print.py
+++ print.py
@@ -1,5 +1,10 @@
 import requests
 
+print(open(__file__).read())
+
+download = requests.get(url='https://raw.githubusercontent.com/ljzhang123/cmput404-lab1/master/print.py')
+open('downloaded-script.py', 'wt').write(download.text)
+
 print('\nrequests library version: ' + requests.__version__)
 
 response = requests.get(url='http://www.google.com/')
